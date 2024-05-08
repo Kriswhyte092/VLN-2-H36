033e3f96e7bba54afc776d780fced57bf8f9a7ab
Kriswhyte092 <kristoferg22@ru.is>
2024-05-08 11:27:22 +0000

    added to staticfiles

--- JamminJobs/JamminJobs/settings.py
+++ JamminJobs/JamminJobs/settings.py
@@ -118,9 +118,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-        BASE_DIR / "static",
-        "/var/www/static/",
+    BASE_DIR/"JJ_app/static",
 ]
+
+STATIC_ROOT = BASE_DIR/"staticfiles"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
