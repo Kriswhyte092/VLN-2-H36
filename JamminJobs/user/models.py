12b26e2b3ac0534f990e093ad789109308f08f17
brekigunnars <147734485+brekigunnars@users.noreply.github.com>
2024-05-10 20:22:45 +0000

    i think the database is finally ready

--- JamminJobs/user/models.py
+++ JamminJobs/user/models.py
@@ -1,19 +1,15 @@
 from django.db import models
 from job.models import Job
-# Create your models here.
-
-class UserPicture(models.Model):
-    profile_pic = models.ImageField()
 
 class User(models.Model):
-    id = models.IntegerField(primary_key=True)
-    ssn = models.IntegerField()
+    id = models.AutoField(primary_key=True)  
+    ssn = models.IntegerField(unique=True)  
     name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     birthdate = models.DateField()
     phone = models.IntegerField()
-    picture = models.ForeignKey(UserPicture, blank=True, on_delete=models.CASCADE)
+    picture = models.CharField(max_length=255, blank=True)
 
 class AppliesTo(models.Model):
-    job_id = models.ForeignKey(Job, to_field='id', on_delete=models.CASCADE)
+    job_id = models.ForeignKey(Job, on_delete=models.CASCADE)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
