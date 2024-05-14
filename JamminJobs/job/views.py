b4f527398153fa0edc967e500bd13ac6c99a56d7
brekigunnars <147734485+brekigunnars@users.noreply.github.com>
2024-05-14 01:07:10 +0000

    i am losing my fucking shit

--- JamminJobs/job/views.py
+++
@@ -1,9 +0,0 @@
-# views.py
-from django.shortcuts import render
-from job.models import Job
-
-def home(request):
-    jobs = Job.objects.all()
-    context = {'job_titles': jobs}  # Corrected key to 'job_titles'
-    return render(request, 'jobs/job.html', context)
-
