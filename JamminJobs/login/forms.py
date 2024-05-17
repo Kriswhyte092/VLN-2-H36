f8dfe4405de3122437a53ad3c9b0f16815405096
Kriswhyte092 <kristoferg22@ru.is>
2024-05-17 05:46:19 +0000

    she gave me a fake id and it worked

--- JamminJobs/login/forms.py
+++ JamminJobs/login/forms.py
@@ -1,35 +1,17 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from user.models import CustomUser
 
 
-class CustomUserCreationForm(UserCreationForm):
-    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
-    
+class CreateUserForm(UserCreationForm):
     class Meta:
-        model = CustomUser
-        fields = ['username', 'password1', 'password2', 'email', 'name', 'birthdate', 'phone', 'picture']
-        labels = {
-            'username': 'Username',
-            'email': 'Email',
-            'name': 'Full Name',
-            'birthdate': 'Birthdate',
-            'phone': 'Phone',
-            'picture': 'Profile Picture'
-        }
-        help_texts = {
-            'username': 'Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.',
-            'email': 'Required. Must be a valid email address.',
-            'birthdate': 'Required. Format: YYYY-MM-DD',
-            'phone': 'Optional. Enter phone number in format XXX-XXX-XXXX.',
-            'picture': 'Optional. Upload a profile picture.'
-        }
-    
-    def clean_email(self):
-        email = self.cleaned_data.get('email')
-        if CustomUser.objects.filter(email=email).exists():
-            raise forms.ValidationError('This email is already in use.')
-        return email
-
-
+        model = User
+        fields = [
+            'first_name',
+            'last_name',
+            'username',
+            'email',
+            'password1',
+            'password2'
+        ]
--- JamminJobs/login/views.py
+++ JamminJobs/login/views.py
@@ -1,24 +1,25 @@
 from django.shortcuts import render, redirect
-from .forms import CustomUserCreationForm
 from django.contrib import messages
 from django.contrib.auth import login as auth_login
+from django.contrib.auth.forms import UserCreationForm
+
+from .forms import CreateUserForm
+
 
 def login(request):
     return render(request, 'login/login_page.html')
 
 
 def signup(request):
+    form = CreateUserForm()
+
     if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
+        form = CreateUserForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            auth_login(request, user)  # Use auth_login to log in the user
-            messages.success(request, 'Registration successful. You are now logged in.')
-            return redirect('homeindex')  # Redirect to the desired page after successful registration
-        else:
-            messages.error(request, 'Registration failed. Please correct the errors.')
-    else:
-        form = CustomUserCreationForm()
-    return render(request, 'login/signup_page.html', {'form': form})
+            form.save()
+
+    context = {'form': form}
+    return render(request, 'login/signup_page.html', context)
+
 
 
