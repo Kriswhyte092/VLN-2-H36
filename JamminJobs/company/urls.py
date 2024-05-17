31876101245e5b2f639e6f4340ffe76cc255a439
brekigunnars <147734485+brekigunnars@users.noreply.github.com>
2024-05-17 18:10:06 +0000

    This is epic

--- JamminJobs/company/urls.py
+++ JamminJobs/company/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path("", views.home, name="companyindex")
+        path("", views.home, name="companyindex"),
+        path('<int:company_id>/', views.company_detail, name='company_detail'),
 ]
--- JamminJobs/company/views.py
+++ JamminJobs/company/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from company.models import Company
+from django.shortcuts import render, get_object_or_404
 
 def home(request):
     companies = Company.objects.all()
@@ -10,3 +11,11 @@ def home(request):
 def companyindex(request):
     companies = Company.objects.all()
     return render(request, 'companyindex.html', {'companies': companies})
+
+
+
+def company_detail(request, company_id):
+    company = get_object_or_404(Company, id=company_id)
+    return render(request, 'companies/company_detail.html', {'company': company})
+
+
